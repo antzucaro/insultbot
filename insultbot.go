80812abf1d3abe0a9f0c9a367a830a170b6efbf0
Ant Zucaro <azucaro@gmail.com>
2014-07-18 17:51:42 -0400

    Use CUdyin's loadInsults. gofmt.

--- insultbot.go
+++ insultbot.go
@@ -1,77 +1,67 @@
 package main
 
 import (
-    "bufio"
-    "github.com/thoj/go-ircevent"
-    "flag"
-    "fmt"
-    "math/rand"
-    "os"
-    "strings"
-    "time"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+
+	"github.com/thoj/go-ircevent"
 )
 
 func loadInsults(fn string) []string {
-    f, err := os.Open(fn)
-    if err != nil {
-        fmt.Println("Couldn't open " + fn + ". Using a default insult instead.")
-        return []string{"You suck!"}
-    }
-    defer f.Close()
+	data, err := ioutil.ReadFile(fn)
+	if err != nil {
+		fmt.Println("can't open", fn, ", using default insult instead")
+		data = []byte("You suck!")
+	}
 
-    r := bufio.NewReader(f)
-    insults := make([]string, 0)
-
-    line, err := r.ReadString('\n')
-    for err == nil {
-        insults = append(insults, line)
-        line, err = r.ReadString('\n')
-    }
-
-    return insults
+	return strings.Split(string(data), "\n")
 }
 
 func main() {
-    file := flag.String("file", "insults.txt", "A text file with insults")
-    room := flag.String("chan", "#hoctf.test", "Channel to join")
-    flag.Parse()
+	file := flag.String("file", "insults.txt", "A text file with insults")
+	room := flag.String("chan", "#hoctf.test", "Channel to join")
+	flag.Parse()
 
-    // random seed
-    t := time.Now()
-    seed := t.Unix()
-    rand.Seed(seed)
+	// random seed
+	t := time.Now()
+	seed := t.Unix()
+	rand.Seed(seed)
 
-    insults := loadInsults(*file)
+	insults := loadInsults(*file)
 
-    // connect
-    conn := irc.IRC("InsultBot", "InsultBot")
-    err := conn.Connect("irc.quakenet.org:6667")
-    if err != nil {
-        fmt.Println("Could not connect!")
-        return
-    }
+	// connect
+	conn := irc.IRC("InsultBot", "InsultBot")
+	err := conn.Connect("irc.quakenet.org:6667")
+	if err != nil {
+		fmt.Println("Could not connect!")
+		return
+	}
 
-    // join our chosen room
-    conn.AddCallback("001", func(e *irc.Event) {
-        conn.Join(*room)
-        conn.Privmsg(*room, "Hi, I'm InsultBot. Say 'insult <nick>' to insult someone!")
-    })
+	// join our chosen room
+	conn.AddCallback("001", func(e *irc.Event) {
+		conn.Join(*room)
+		conn.Privmsg(*room, "Hi, I'm InsultBot. Say 'insult <nick>' to insult someone!")
+	})
 
-    // and now for the insults!
-    conn.AddCallback("PRIVMSG", func (e *irc.Event) {
-        msg := e.Message()
-        if len(msg) >= 6 && strings.ToLower(msg[0:6]) == "insult" {
-            tokens := strings.Split(msg, " ")
+	// and now for the insults!
+	conn.AddCallback("PRIVMSG", func(e *irc.Event) {
+		msg := e.Message()
+		if len(msg) >= 6 && strings.ToLower(msg[0:6]) == "insult" {
+			tokens := strings.Split(msg, " ")
 
-            if len(tokens) > 1 {
-                nick := tokens[1]
-                // check here if the nick actually exists?
-                insult := insults[rand.Intn(len(insults))]
+			if len(tokens) > 1 {
+				nick := tokens[1]
+				// check here if the nick actually exists?
+				insult := insults[rand.Intn(len(insults))]
 
-                conn.Privmsg(*room, nick + ": " + insult)
-            }
-        }
-    })
+				conn.Privmsg(*room, nick+": "+insult)
+			}
+		}
+	})
 
-    conn.Loop()
+	conn.Loop()
 }
